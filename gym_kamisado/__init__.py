8140a8f6d40712469ba78ab6a6ed040054878ec9
Bigeco <bigeco173@gmail.com>
2024-06-09 16:26:17 +0900

    Feat: add DQN train code

--- gym_kamisado/__init__.py
+++ gym_kamisado/__init__.py
@@ -3,7 +3,6 @@ from gymnasium.envs.registration import register
 register(
     id="Kamisado-v0",
     entry_point="gym_kamisado.envs:KamisadoEnv",
-    autoreset=True,
     order_enforce=True,
     max_episode_steps=100,
     reward_threshold=1,
