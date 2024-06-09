00c4823d9c518c121d64be9b2279d9d33677f720
Bigeco <bigeco173@gmail.com>
2024-06-09 16:22:23 +0900

    Rename: change agent.py to ai_agents.py

---
+++ gym_kamisado/agents/ai_agents.py
@@ -0,0 +1,133 @@
+import os
+
+import random
+from collections import deque
+
+import numpy as np
+from tensorflow import keras
+from keras import models
+from keras import layers
+from keras import optimizers
+
+class BaseAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+           
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        return self.select_action(state)
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            self.learn(state, action, reward, next_state, done)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        raise NotImplementedError
+
+    def save(self, name):
+        raise NotImplementedError
+
+
+class DQNAgent(BaseAgent):
+    def __init__(self, state_size, action_size):
+        super().__init__(state_size, action_size)
+        self.weight_backup = "kamisado_DQN_weight.h5"
+        self.exploration_rate = 1.0
+        self.exploration_min = 0.01
+        self.exploration_decay = 0.995
+        self.model = self._build_model()
+
+    def _build_model(self, use_previous_saved_weight=False): 
+        model = models.Sequential()
+        model.add(layers.Dense(128, input_dim=self.state_size, activation='relu'))
+        model.add(layers.Dense(128, activation='relu'))
+        model.add(layers.Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=optimizers.Adam(learning_rate=self.learning_rate))
+        if os.path.isfile(self.weight_backup) and use_previous_saved_weight:
+            model.load_weights(self.weight_backup)
+            self.exploration_rate = self.exploration_min
+        return model
+
+    def save_model(self):
+        self.model.save('gym_kamisado/model/' + self.weight_backup)
+
+    def act(self, state):
+        if np.random.rand() <= self.exploration_rate:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+        
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+              target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.exploration_rate > self.exploration_min:
+            self.exploration_rate *= self.exploration_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+
+class QLearningAgent(BaseAgent):
+    def __init__(self, state_size, action_size):
+        super().__init__(state_size, action_size)
+        self.q_table = np.zeros((state_size, action_size))
+
+    def select_action(self, state):
+        return np.argmax(self.q_table[state])
+
+    def learn(self, state, action, reward, next_state, done):
+        best_next_action = np.argmax(self.q_table[next_state])
+        target = reward + self.gamma * self.q_table[next_state][best_next_action] * (not done)
+        self.q_table[state][action] += self.learning_rate * (target - self.q_table[state][action])
+
+    def load(self, name):
+        self.q_table = np.load(name)
+
+    def save(self, name):
+        np.save(name, self.q_table)
+
+
+class SARSAAgent(BaseAgent):
+    def __init__(self, state_size, action_size):
+        super().__init__(state_size, action_size)
+        self.q_table = np.zeros((state_size, action_size))
+
+    def select_action(self, state):
+        return np.argmax(self.q_table[state])
+
+    def learn(self, state, action, reward, next_state, next_action, done):
+        target = reward + self.gamma * self.q_table[next_state][next_action] * (not done)
+        self.q_table[state][action] += self.learning_rate * (target - self.q_table[state][action])
+
+    def load(self, name):
+        self.q_table = np.load(name)
+
+    def save(self, name):
+        np.save(name, self.q_table)
