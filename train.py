da617d921fbc4b92eb7577fece6b61a71798cf19
Bigeco <bigeco173@gmail.com>
2024-06-11 00:22:08 +0900

    Fix: resolve error to save and load keras model

--- train.py
+++ train.py
@@ -38,8 +38,8 @@ def train_dqn_agent(episodes=100, batch_size=32, learning_rate=0.01, discount_fa
                 dqn_agent.replay(batch_size)
 
             print(f"episode: {e+1}/{episodes}, score: {reward}, epsilon: {dqn_agent.epsilon}")
-        dqn_agent.save('gym_kamisado/agents/model/' + 'kamisado_dqn_model.weights.keras')
-        #dqn_agent.save_model('./gym_kamisado/agents/model/')
+
+        dqn_agent.save_model()
 
 def train_qlearning_agent(episodes=1000, batch_size=32, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_min=0.01, epsilon_decay=0.995):
     env = gym.make('Kamisado-v0', render_mode="rgb_array")
@@ -103,6 +103,6 @@ def train_sarsa_agent(episodes=100):
 
 
 if __name__ == "__main__":
-    train_dqn_agent(episodes=20)
+    train_dqn_agent(episodes=10)
+    # train_qlearning_agent()
     # train_sarsa_agent()
-    pass
